b76c25f45c07d5c4c71be9886011f4d9c9a40a70
Wing Chi Chan <w.chi.chan@gmail.com>
2025-01-17 10:22:02 +0000

    forEach function added and working

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const imageArray = [
   {
     src: "assets/image1.jpeg",
-    alt: "sunset over a vast ocean with mountainous island in the background",
+    alt: "sunset over a vast ocean with an island in the background",
+  },
+  {
+    src: "assets/image2.jpeg",
+    alt: "sunset over water with fishing boats",
+  },
+  {
+    src: "assets/image3.jpeg",
+    alt: "Goa 2024 written in the sand",
+  },
+  {
+    src: "assets/image4.jpeg",
+    alt: "4 teepees in twilight sun",
   },
 ];
 
-// first object in the array
-const imageObject = imageArray[0];
+imageArray.forEach(function (object) {
+  const imgElm = document.createElement("img");
+  imgElm.src = object.src;
+  imgElm.alt = object.alt;
+  document.body.append(imgElm);
+});
 
+// to display each image in the array
+
+// first object in the array
+// const imageObject = imageArray;
 // create image element and assign src and alt
-const imgElm = document.createElement("img");
+
 // assign src to image
-imgElm.src = imageObject.src;
-imgElm.alt = imageObject.alt;
 
 // add element to page
-document.body.append(imgElm);
