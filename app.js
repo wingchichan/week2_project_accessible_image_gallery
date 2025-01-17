507e63937f923d63cf9a274d2a1a2a0a20be5991
Wing Chi Chan <w.chi.chan@gmail.com>
2025-01-17 13:15:29 +0000

    big image displaying wen clicked

--- app.js
+++ app.js
@@ -17,14 +17,25 @@ const imageArray = [
   },
 ];
 
-// to display each image in the array
-// for every object in the imageArray
-// create an img element tag in the document and store that in imgElm variable
-// link the variable src and alt to the src and alt in the array objects
-// append each variable to the body of the document
-imageArray.forEach(function (object) {
-  const imgElm = document.createElement("img");
-  imgElm.src = object.src;
-  imgElm.alt = object.alt;
-  document.body.append(imgElm);
-});
+function generateThumbnails() {
+  // to display each image in the array
+  // for every object in the imageArray
+  // create an img element tag in the document and store that in imgElm variable
+  // link the variable src and alt to the src and alt in the array objects
+  // append each variable to the body of the document
+  imageArray.forEach(function (object) {
+    // create tag <img />
+    const imgElm = document.createElement("img");
+    imgElm.src = object.src;
+    imgElm.alt = object.alt;
+
+    document.getElementById("thumbnail").append(imgElm);
+
+    // add event listener to each image
+    imgElm.addEventListener("click", function () {
+      document.getElementById("image").src = object.src;
+    });
+  });
+}
+
+generateThumbnails();
